48a0bbe9fca4d3b348ae172fe3620473359bef46
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-03 21:09:41 +0100

    make circles appear on click and not double in same box

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-let Gameboard = {
+let Gameboard = () => {
   let courseOfGame = [];
+  return result;
+};
+
+let placeMarks = () => {
+  let placement;
+  return placement;
+};
+
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box4 = document.getElementById('box4');
+let box5 = document.getElementById('box5');
+let box3 = document.getElementById('box3');
+let box6 = document.getElementById('box6');
+let box7 = document.getElementById('box7');
+let box8 = document.getElementById('box8');
+let box9 = document.getElementById('box9');
+
+function createMarks() {
+  let circle = document.createElement('img');
+  let xMark = document.createElement('img');
+
+  circle.className += 'circle ';
+  circle.className += 'game-mark';
+  circle.setAttribute('src', './assets/circle.png');
+
+  xMark.className += 'x-mark ';
+  xMark.className += 'game-mark';
+  xMark.setAttribute('src', './assets/x-mark.png');
+
+  return {
+    xMark, circle,
+  };
+}
+
+for (let i = 1; i <= 9; i++) {
+  let playerXisOn;
+  let playerOisOn = true;
+  playerOisOn ? playerXisOn = false : playerXisOn;
+  playerXisOn ? playerOisOn = false : playerOisOn;
+  let whichBox = `box${i}`;
+  this[whichBox].addEventListener('click', () => {
+    let mark = createMarks();
+    if (this[whichBox].hasChildNodes()) {
+      console.log('Clicked again in the same box. Doing nothing.');
+    } else {
+      playerOisOn ? this[whichBox].appendChild(mark.circle) : this[whichBox].appendChild(mark.xMark);
+    }
+  });
+  playerOisOn ? playerXisOn : playerOisOn;
 }
