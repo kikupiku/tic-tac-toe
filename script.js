187a0dad2d760ccb9555c124eef52f2899af9d49
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-04 02:17:28 +0100

    figure out bug 'classList' property of null, solve in comment

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ let box8 = document.getElementById('box8');
 let box9 = document.getElementById('box9');
 
 function createMarks() {
-  let circle = document.createElement('img');
+  let oMark = document.createElement('img');
   let xMark = document.createElement('img');
 
-  circle.className += 'circle ';
-  circle.className += 'game-mark';
-  circle.setAttribute('src', './assets/circle.png');
+  oMark.className += 'circle ';
+  oMark.className += 'game-mark';
+  oMark.setAttribute('src', './assets/circle.png');
 
   xMark.className += 'x-mark ';
   xMark.className += 'game-mark';
   xMark.setAttribute('src', './assets/x-mark.png');
 
   return {
-    xMark, circle,
+    xMark, oMark,
   };
 }
 
@@ -51,7 +51,7 @@ function drawOnBoard() {
       if (this[whichBox].hasChildNodes()) {
         console.log('Clicked again in the same box. Doing nothing.');
       } else {
-        playerOisOn ? this[whichBox].appendChild(mark.circle)
+        playerOisOn ? this[whichBox].appendChild(mark.oMark)
               : this[whichBox].appendChild(mark.xMark);
       }
 
@@ -62,6 +62,8 @@ function drawOnBoard() {
         playerOisOn = true;
         playerXisOn = false;
       }
+
+      checkIfWin();
     });
 
   }
@@ -69,6 +71,56 @@ function drawOnBoard() {
 
 drawOnBoard();
 
-let winCondition = () => {
-  
-}
+function checkIfWin() {
+  let top = document.getElementById('top-hor');
+  let midHorizontal = document.getElementById('mid-hor');
+  let bottom = document.getElementById('bot-hor');
+  let left = document.getElementById('left-vert');
+  let midVertical = document.getElementById('mid-vert');
+  let right = document.getElementById('right-vert');
+  let downToRight = document.getElementById('across-down-to-right');
+  let upToRight = document.getElementById('across-up-to-right');
+
+  function checkBoxChildren() {
+    let box1mark = box1.firstChild.classList;
+    let box2mark = box2.firstChild.classList;
+    let box3mark = box3.firstChild.classList;
+    let box4mark = box4.firstChild.classList;
+    let box5mark = box5.firstChild.classList;
+    let box6mark = box6.firstChild.classList;
+    let box7mark = box7.firstChild.classList;
+    let box8mark = box8.firstChild.classList;
+    let box9mark = box9.firstChild.classList;
+
+    return {
+      box1mark, box2mark, box3mark, box4mark, box5mark, box6mark, box7mark, box8mark, box9mark,
+    };
+    // the way to repair it - all boxes should have empty img elements and only fill them with src upon click, that way no null will come up
+  }
+  let boxChild = checkBoxChildren();
+
+  if (box1.hasChildNodes && box2.hasChildNodes && box3.hasChildNodes) {
+    console.log('they all have children');
+    if ((boxChild.box1mark.contains('circle')) &&
+         (boxChild.box2mark.contains('circle')) &&
+         (boxChild.box3mark.contains('circle'))) {
+      console.log('o wins');
+      top.style.display = 'block';
+    }
+  } else {
+    console.log('they do not all have children');
+  }
+
+  // } else if ((box4mark.contains('circle')) &&
+  //            (box5mark.contains('circle')) &&
+  //            (box6mark.contains('circle'))) {
+  //   midHorizontal.style.display = 'block';
+  // } else if ((box7mark.contains('circle')) &&
+  //            (box8mark.contains('circle')) &&
+  //            (box9mark.contains('circle'))) {
+  //   bottom.style.display = 'block';
+  // } else if ((box1mark.contains('circle')) &&
+  //            (box4mark.contains('circle')) &&
+  //            (box7mark.contains('circle'))) {
+  //   left.style.display = 'block';
+};
