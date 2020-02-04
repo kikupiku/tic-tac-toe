f27fdedca0ffc2b46fe8b46990ed162d53dc996a
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-04 15:19:05 +0100

    fix bug that only showed win line if all boxes ticked

--- script.js
+++ script.js
@@ -18,22 +18,23 @@ let box7 = document.getElementById('box7');
 let box8 = document.getElementById('box8');
 let box9 = document.getElementById('box9');
 
-function createMarks() {
-  let oMark = document.createElement('img');
-  let xMark = document.createElement('img');
-
-  oMark.className += 'circle ';
-  oMark.className += 'game-mark';
-  oMark.setAttribute('src', './assets/circle.png');
-
-  xMark.className += 'x-mark ';
-  xMark.className += 'game-mark';
-  xMark.setAttribute('src', './assets/x-mark.png');
-
-  return {
-    xMark, oMark,
-  };
-}
+// function createMarks() {
+//
+//   //   let oMark =
+//   //   let xMark =
+//   // this[whichBox].firstChild.setAttribute('src', './assets/circle.png');
+//   // this[whichBox].firstChild.setAttribute('src', './assets/x-mark.png');
+//
+//   oMark.className += 'circle ';
+//   oMark.className += 'game-mark';
+//
+//   xMark.className += 'x-mark ';
+//   xMark.className += 'game-mark';
+//
+//   return {
+//     xMark, oMark,
+//   };
+// }
 
 function drawOnBoard() {
   let playerXisOn = false;
@@ -42,17 +43,26 @@ function drawOnBoard() {
   for (let i = 1; i <= 9; i++) {
 
     let whichBox = `box${i}`;
-
     this[whichBox].addEventListener('click', () => {
       playerOisOn ? playerXisOn = false : playerXisOn;
       playerXisOn ? playerOisOn = false : playerOisOn;
-      let mark = createMarks();
 
-      if (this[whichBox].hasChildNodes()) {
+      // let mark = createMarks();
+      console.log(this[whichBox].firstElementChild);
+      if (this[whichBox].firstElementChild.hasAttribute('src')) {
         console.log('Clicked again in the same box. Doing nothing.');
       } else {
-        playerOisOn ? this[whichBox].appendChild(mark.oMark)
-              : this[whichBox].appendChild(mark.xMark);
+        if (playerOisOn) {
+          let oMark = this[whichBox].firstElementChild;
+          oMark.setAttribute('src', './assets/circle.png');
+          oMark.className += 'circle ';
+          oMark.className += 'game-mark';
+        } else {
+          let xMark = this[whichBox].firstElementChild;
+          xMark.setAttribute('src', './assets/x-mark.png');
+          xMark.className += 'x-mark ';
+          xMark.className += 'game-mark';
+        }
       }
 
       if (playerOisOn) {
@@ -82,33 +92,33 @@ function checkIfWin() {
   let upToRight = document.getElementById('across-up-to-right');
 
   function checkBoxChildren() {
-    let box1mark = box1.firstChild.classList;
-    let box2mark = box2.firstChild.classList;
-    let box3mark = box3.firstChild.classList;
-    let box4mark = box4.firstChild.classList;
-    let box5mark = box5.firstChild.classList;
-    let box6mark = box6.firstChild.classList;
-    let box7mark = box7.firstChild.classList;
-    let box8mark = box8.firstChild.classList;
-    let box9mark = box9.firstChild.classList;
+    let box1mark = box1.firstElementChild.classList;
+    let box2mark = box2.firstElementChild.classList;
+    let box3mark = box3.firstElementChild.classList;
+    let box4mark = box4.firstElementChild.classList;
+    let box5mark = box5.firstElementChild.classList;
+    let box6mark = box6.firstElementChild.classList;
+    let box7mark = box7.firstElementChild.classList;
+    let box8mark = box8.firstElementChild.classList;
+    let box9mark = box9.firstElementChild.classList;
 
     return {
       box1mark, box2mark, box3mark, box4mark, box5mark, box6mark, box7mark, box8mark, box9mark,
     };
-    // the way to repair it - all boxes should have empty img elements and only fill them with src upon click, that way no null will come up
+
+    // the way to repair it - all boxes should have empty img elements and only
+    // fill them with src upon click, that way no null will come up
+
   }
+
   let boxChild = checkBoxChildren();
 
-  if (box1.hasChildNodes && box2.hasChildNodes && box3.hasChildNodes) {
-    console.log('they all have children');
-    if ((boxChild.box1mark.contains('circle')) &&
-         (boxChild.box2mark.contains('circle')) &&
-         (boxChild.box3mark.contains('circle'))) {
-      console.log('o wins');
-      top.style.display = 'block';
-    }
-  } else {
-    console.log('they do not all have children');
+
+  if ((boxChild.box1mark.contains('circle')) &&
+       (boxChild.box2mark.contains('circle')) &&
+       (boxChild.box3mark.contains('circle'))) {
+    console.log('o wins');
+    top.style.display = 'block';
   }
 
   // } else if ((box4mark.contains('circle')) &&
