38b8bf71ab6d878e59655ff9338a2386eb0172c5
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-04 17:19:17 +0100

    make one round game fully functional

--- script.js
+++ script.js
@@ -10,31 +10,13 @@ let placeMarks = () => {
 
 let box1 = document.getElementById('box1');
 let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
 let box4 = document.getElementById('box4');
 let box5 = document.getElementById('box5');
-let box3 = document.getElementById('box3');
 let box6 = document.getElementById('box6');
 let box7 = document.getElementById('box7');
 let box8 = document.getElementById('box8');
 let box9 = document.getElementById('box9');
-
-// function createMarks() {
-//
-//   //   let oMark =
-//   //   let xMark =
-//   // this[whichBox].firstChild.setAttribute('src', './assets/circle.png');
-//   // this[whichBox].firstChild.setAttribute('src', './assets/x-mark.png');
-//
-//   oMark.className += 'circle ';
-//   oMark.className += 'game-mark';
-//
-//   xMark.className += 'x-mark ';
-//   xMark.className += 'game-mark';
-//
-//   return {
-//     xMark, oMark,
-//   };
-// }
 
 function drawOnBoard() {
   let playerXisOn = false;
@@ -47,9 +29,8 @@ function drawOnBoard() {
       playerOisOn ? playerXisOn = false : playerXisOn;
       playerXisOn ? playerOisOn = false : playerOisOn;
 
-      // let mark = createMarks();
       console.log(this[whichBox].firstElementChild);
-      if (this[whichBox].firstElementChild.hasAttribute('src')) {
+      if (this[whichBox].firstElementChild.hasAttribute('class')) {
         console.log('Clicked again in the same box. Doing nothing.');
       } else {
         if (playerOisOn) {
@@ -82,55 +63,109 @@ function drawOnBoard() {
 drawOnBoard();
 
 function checkIfWin() {
-  let top = document.getElementById('top-hor');
-  let midHorizontal = document.getElementById('mid-hor');
-  let bottom = document.getElementById('bot-hor');
-  let left = document.getElementById('left-vert');
-  let midVertical = document.getElementById('mid-vert');
-  let right = document.getElementById('right-vert');
-  let downToRight = document.getElementById('across-down-to-right');
-  let upToRight = document.getElementById('across-up-to-right');
 
-  function checkBoxChildren() {
-    let box1mark = box1.firstElementChild.classList;
-    let box2mark = box2.firstElementChild.classList;
-    let box3mark = box3.firstElementChild.classList;
-    let box4mark = box4.firstElementChild.classList;
-    let box5mark = box5.firstElementChild.classList;
-    let box6mark = box6.firstElementChild.classList;
-    let box7mark = box7.firstElementChild.classList;
-    let box8mark = box8.firstElementChild.classList;
-    let box9mark = box9.firstElementChild.classList;
+  function getWinLine(idString) {
+    let line = document.getElementById(idString);
 
     return {
-      box1mark, box2mark, box3mark, box4mark, box5mark, box6mark, box7mark, box8mark, box9mark,
+      line,
     };
-
-    // the way to repair it - all boxes should have empty img elements and only
-    // fill them with src upon click, that way no null will come up
-
   }
 
-  let boxChild = checkBoxChildren();
+  function checkMark(box, mark) {
+    let _boxMark = box.firstElementChild.classList;
+    let fullBox = _boxMark.contains(mark);
 
-
-  if ((boxChild.box1mark.contains('circle')) &&
-       (boxChild.box2mark.contains('circle')) &&
-       (boxChild.box3mark.contains('circle'))) {
-    console.log('o wins');
-    top.style.display = 'block';
+    return {
+      fullBox,
+    };
   }
 
-  // } else if ((box4mark.contains('circle')) &&
-  //            (box5mark.contains('circle')) &&
-  //            (box6mark.contains('circle'))) {
-  //   midHorizontal.style.display = 'block';
-  // } else if ((box7mark.contains('circle')) &&
-  //            (box8mark.contains('circle')) &&
-  //            (box9mark.contains('circle'))) {
-  //   bottom.style.display = 'block';
-  // } else if ((box1mark.contains('circle')) &&
-  //            (box4mark.contains('circle')) &&
-  //            (box7mark.contains('circle'))) {
-  //   left.style.display = 'block';
-};
+  function triggerWin(player) {
+    let _winDiv = document.getElementById('win-message');
+    _winDiv.innerHTML = `${player} wins!`;
+    _winDiv.style.display = 'block';
+  }
+
+  if ((checkMark(box1, 'circle').fullBox) &&
+      (checkMark(box2, 'circle').fullBox) &&
+      (checkMark(box3, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('top-hor').line.style.display = 'block';
+  } else if ((checkMark(box4, 'circle').fullBox) &&
+             (checkMark(box5, 'circle').fullBox) &&
+             (checkMark(box6, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('mid-hor').line.style.display = 'block';
+  } else if ((checkMark(box7, 'circle').fullBox) &&
+             (checkMark(box8, 'circle').fullBox) &&
+             (checkMark(box9, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('bot-hor').line.style.display = 'block';
+  } else if ((checkMark(box1, 'circle').fullBox) &&
+             (checkMark(box4, 'circle').fullBox) &&
+             (checkMark(box7, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('left-vert').line.style.display = 'block';
+  } else if ((checkMark(box2, 'circle').fullBox) &&
+             (checkMark(box5, 'circle').fullBox) &&
+             (checkMark(box8, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('mid-vert').line.style.display = 'block';
+  } else if ((checkMark(box3, 'circle').fullBox) &&
+             (checkMark(box6, 'circle').fullBox) &&
+             (checkMark(box9, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('right-vert').line.style.display = 'block';
+  } else if ((checkMark(box1, 'circle').fullBox) &&
+             (checkMark(box5, 'circle').fullBox) &&
+             (checkMark(box9, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('down-to-right').line.style.display = 'block';
+  } else if ((checkMark(box3, 'circle').fullBox) &&
+             (checkMark(box5, 'circle').fullBox) &&
+             (checkMark(box7, 'circle').fullBox)) {
+    triggerWin('player O');
+    getWinLine('up-to-right').line.style.display = 'block';
+  } else if ((checkMark(box1, 'x-mark').fullBox) &&
+             (checkMark(box2, 'x-mark').fullBox) &&
+             (checkMark(box3, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('top-hor').line.style.display = 'block';
+  } else if ((checkMark(box4, 'x-mark').fullBox) &&
+             (checkMark(box5, 'x-mark').fullBox) &&
+             (checkMark(box6, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('mid-hor').line.style.display = 'block';
+  } else if ((checkMark(box7, 'x-mark').fullBox) &&
+             (checkMark(box8, 'x-mark').fullBox) &&
+             (checkMark(box9, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('bot-hor').line.style.display = 'block';
+  } else if ((checkMark(box1, 'x-mark').fullBox) &&
+             (checkMark(box4, 'x-mark').fullBox) &&
+             (checkMark(box7, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('left-vert').line.style.display = 'block';
+  } else if ((checkMark(box2, 'x-mark').fullBox) &&
+             (checkMark(box5, 'x-mark').fullBox) &&
+             (checkMark(box8, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('mid-vert').line.style.display = 'block';
+  } else if ((checkMark(box3, 'x-mark').fullBox) &&
+             (checkMark(box6, 'x-mark').fullBox) &&
+             (checkMark(box9, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('right-vert').line.style.display = 'block';
+  } else if ((checkMark(box1, 'x-mark').fullBox) &&
+             (checkMark(box5, 'x-mark').fullBox) &&
+             (checkMark(box9, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('down-to-right').line.style.display = 'block';
+  } else if ((checkMark(box3, 'x-mark').fullBox) &&
+             (checkMark(box5, 'x-mark').fullBox) &&
+             (checkMark(box7, 'x-mark').fullBox)) {
+    triggerWin('player X');
+    getWinLine('up-to-right').line.style.display = 'block';
+  }
+}
